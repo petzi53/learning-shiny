2b5bb4af58bcfe34b4fc3af484af197c6a72bca6
petzi53 <petzi53@gmail.com>
2025-06-06 14:13:54 +0200

    neiss.R without data

--- R/neiss.R
+++ R/neiss.R
@@ -4,15 +4,15 @@
 
 ## data
 
-url_injuries <- paste0(url_basic, "neiss/injuries.tsv.gz")
-url_population <- paste0(url_basic, "neiss/population.tsv")
-url_products <- paste0(url_basic, "neiss/products.tsv")
-
-injuries <-  vroom::vroom(url_injuries, show_col_types = FALSE)
-population <-  vroom::vroom(url_population, show_col_types = FALSE)
-products <-  vroom::vroom(url_products, show_col_types = FALSE)
-
-prod_codes <- setNames(products$prod_code, products$title)
+# url_injuries <- paste0(url_basic, "neiss/injuries.tsv.gz")
+# url_population <- paste0(url_basic, "neiss/population.tsv")
+# url_products <- paste0(url_basic, "neiss/products.tsv")
+#
+# injuries <-  vroom::vroom(url_injuries, show_col_types = FALSE)
+# population <-  vroom::vroom(url_population, show_col_types = FALSE)
+# products <-  vroom::vroom(url_products, show_col_types = FALSE)
+#
+# prod_codes <- stats::setNames(products$prod_code, products$title)
 
 ## function
 count_top <- function(df, var, n = 5) {
